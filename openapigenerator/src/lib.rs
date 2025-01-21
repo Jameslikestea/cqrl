81a1e99f969329012741e2c85fc154afd59d00f0
Jameslikestea <5156202+Jameslikestea@users.noreply.github.com>
2025-01-21 22:31:54 +0000

    adds generation of paths

--- openapigenerator/src/lib.rs
+++ openapigenerator/src/lib.rs
@@ -1,16 +1,20 @@
+use std::{ops::Index, vec};
+
 use indexmap::IndexMap;
 use openapiv3::{
-    ArrayType, BooleanType, Components, Info, License, NumberType, ObjectType, OpenAPI,
-    ReferenceOr, Schema, SchemaData, SchemaKind, StringFormat, StringType, Type,
-    VariantOrUnknownOrEmpty,
+    ArrayType, BooleanType, Components, Header, HeaderStyle, Info, License, MediaType, NumberType,
+    ObjectType, OpenAPI, Operation, PathItem, Paths, ReferenceOr, Response, Responses, Schema,
+    SchemaData, SchemaKind, StringFormat, StringType, Type, VariantOrUnknownOrEmpty,
 };
-use parser::{DataTypes, Model, API};
+use parser::{Command, DataTypes, Model, Query, API};
 
 pub fn generate_openapi_spec(api: API) -> OpenAPI {
     let mut schemas: IndexMap<String, ReferenceOr<Schema>> = IndexMap::new();
+    let mut responses: IndexMap<String, ReferenceOr<Response>> = IndexMap::new();
 
     {
-        generate_models(&mut schemas, api.models);
+        generate_models(&mut schemas, api.models.clone());
+        generate_responses(&mut responses, api.queries.clone());
     }
 
     OpenAPI {
@@ -24,12 +28,184 @@ pub fn generate_openapi_spec(api: API) -> OpenAPI {
             version: String::from("1.0.0"),
             ..Default::default()
         },
+        paths: Paths {
+            paths: generate_paths(api.commands.clone(), api.queries.clone()),
+            ..Default::default()
+        },
         components: Some(Components {
             schemas: schemas,
+            responses: responses,
             ..Default::default()
         }),
         ..Default::default()
     }
+}
+
+fn generate_paths(
+    commands: Vec<Command>,
+    queries: Vec<Query>,
+) -> IndexMap<String, ReferenceOr<PathItem>> {
+    let mut paths = IndexMap::new();
+
+    for command in commands.iter() {
+        paths.insert(
+            format!("/command.{}", command.name),
+            ReferenceOr::Item(PathItem {
+                post: Some(Operation {
+                    responses: Responses {
+                        responses: {
+                            let mut imap = IndexMap::new();
+
+                            imap.insert(
+                                openapiv3::StatusCode::Code(201),
+                                ReferenceOr::ref_("#/components/responses/command.success"),
+                            );
+                            imap.insert(
+                                openapiv3::StatusCode::Code(401),
+                                ReferenceOr::ref_("#/components/responses/command.unauthorized"),
+                            );
+                            imap.insert(
+                                openapiv3::StatusCode::Code(422),
+                                ReferenceOr::ref_("#/components/responses/command.baddata"),
+                            );
+                            imap.insert(
+                                openapiv3::StatusCode::Code(500),
+                                ReferenceOr::ref_("#/components/responses/command.internal"),
+                            );
+
+                            imap
+                        },
+                        ..Default::default()
+                    },
+                    tags: vec!["commands".to_string()],
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+        );
+    }
+
+    for query in queries.iter() {
+        paths.insert(
+            format!("/query.{}", query.name),
+            ReferenceOr::Item(PathItem {
+                get: Some(Operation {
+                    tags: vec!["queries".to_string()],
+                    responses: Responses {
+                        responses: {
+                            let mut responses = IndexMap::new();
+
+                            responses.insert(
+                                openapiv3::StatusCode::Code(401),
+                                ReferenceOr::ref_("#/components/responses/query.unauthorized"),
+                            );
+                            responses.insert(
+                                openapiv3::StatusCode::Code(404),
+                                ReferenceOr::ref_("#/components/responses/query.notfound"),
+                            );
+                            responses.insert(
+                                openapiv3::StatusCode::Code(500),
+                                ReferenceOr::ref_("#/components/responses/query.internal"),
+                            );
+
+                            responses
+                        },
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+        );
+    }
+
+    paths
+}
+
+fn generate_responses(
+    responses: &mut IndexMap<String, ReferenceOr<Response>>,
+    queries: Vec<Query>,
+) {
+    responses.insert(
+        "command.success".to_string(),
+        ReferenceOr::Item(Response {
+            description: "Server accepted the command".to_string(),
+            headers: {
+                let mut imap = IndexMap::new();
+
+                imap.insert(
+                    "X-Operation-Id".to_string(),
+                    ReferenceOr::Item(Header {
+                        deprecated: None,
+                        description: None,
+                        example: None,
+                        examples: IndexMap::new(),
+                        extensions: IndexMap::new(),
+                        format: openapiv3::ParameterSchemaOrContent::Schema(ReferenceOr::Item(
+                            Schema {
+                                schema_data: SchemaData::default(),
+                                schema_kind: SchemaKind::Type(Type::String(StringType::default())),
+                            },
+                        )),
+                        required: false,
+                        style: HeaderStyle::Simple,
+                    }),
+                );
+
+                imap
+            },
+            ..Default::default()
+        }),
+    );
+
+    responses.insert(
+        "command.unauthorized".to_string(),
+        ReferenceOr::Item(Response {
+            description: "Server rejected the command, user was not authorized".to_string(),
+            ..Default::default()
+        }),
+    );
+
+    responses.insert(
+        "command.baddata".to_string(),
+        ReferenceOr::Item(Response {
+            description: "Server rejected the command, data was not valid for that endpoint"
+                .to_string(),
+            ..Default::default()
+        }),
+    );
+
+    responses.insert(
+        "command.internal".to_string(),
+        ReferenceOr::Item(Response {
+            description: "Server rejected the command, something happened internally".to_string(),
+            ..Default::default()
+        }),
+    );
+
+    responses.insert(
+        "query.unauthorized".to_string(),
+        ReferenceOr::Item(Response {
+            description: "Server rejected the query, user not authorized to see that".to_string(),
+            ..Default::default()
+        }),
+    );
+
+    responses.insert(
+        "query.notfound".to_string(),
+        ReferenceOr::Item(Response {
+            description: "Server rejected the query, requested data not found".to_string(),
+            ..Default::default()
+        }),
+    );
+
+    responses.insert(
+        "query.internal".to_string(),
+        ReferenceOr::Item(Response {
+            description: "Server rejected the query, something happened internally".to_string(),
+            ..Default::default()
+        }),
+    );
 }
 
 fn generate_models(schemas: &mut IndexMap<String, ReferenceOr<Schema>>, models: Vec<Model>) {
