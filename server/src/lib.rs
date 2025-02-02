4028d42ce7e76c63defbb98d0841420230b13376
Jameslikestea <5156202+Jameslikestea@users.noreply.github.com>
2025-02-02 22:18:11 +0000

    added query and command paths

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -7,37 +7,52 @@ use hyper::{
     Request, Response,
 };
 use hyper_util::rt::TokioIo;
+use parser::API;
 use std::{error::Error, net::SocketAddr, pin::Pin};
 use tokio::net::TcpListener;
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct Server {
     port: u16,
+    api: API,
 }
 
 impl Server {
     pub fn new() -> Self {
-        Server { port: 8912 }
+        Server {
+            port: 8912,
+            api: API::new(),
+        }
     }
 
     pub fn with_port(&mut self, port: u16) {
         self.port = port;
     }
 
+    pub fn with_api(&mut self, api: API) {
+        self.api = api;
+    }
+
     pub async fn serve(&self) -> Result<(), Box<dyn Error>> {
         let addr = SocketAddr::from(([0, 0, 0, 0], self.port));
         let listener = TcpListener::bind(addr).await?;
+        let api = self.api.clone();
+
+        for command in api.commands.iter() {
+            println!("Discovered Command: {}", command.name);
+        }
+        for query in api.queries.iter() {
+            println!("Discoviered Query: {}", query.name);
+        }
 
         loop {
+            let handler = Handler::new(api.clone());
             let (stream, _) = listener.accept().await?;
 
             let io = TokioIo::new(stream);
 
             tokio::task::spawn(async move {
-                if let Err(error) = http1::Builder::new()
-                    .serve_connection(io, Handler::new())
-                    .await
-                {
+                if let Err(error) = http1::Builder::new().serve_connection(io, handler).await {
                     eprintln!("Error serving connection: {:?}", error);
                 }
             });
@@ -45,29 +60,43 @@ impl Server {
     }
 }
 
-struct Handler {}
+struct Handler<'a> {
+    _name: &'a str,
+    _api: API,
+}
 
-impl Handler {
-    fn new() -> Self {
-        Handler {}
+impl<'a> Handler<'a> {
+    fn new(api: API) -> Self {
+        Handler {
+            _name: "handler",
+            _api: api,
+        }
     }
 }
 
 type Req = Request<Incoming>;
 
-impl Service<Req> for Handler {
+impl<'a> Service<Req> for Handler<'a> {
     type Response = Response<Full<Bytes>>;
 
     type Error = hyper::Error;
 
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'a>>;
 
     fn call(&self, req: Req) -> Self::Future {
         let fut = async move {
-            println!(
-                "Serving request {}",
-                req.uri().path_and_query().unwrap().as_str()
-            );
+            match req.uri().path() {
+                "/command." => {
+                    println!("Running Command");
+                }
+                "/query." => {
+                    println!("Running Query");
+                }
+                path => {
+                    println!("Running Path: {}", path);
+                }
+            }
+
             Ok(Response::new(Full::new(Bytes::from("hi, world"))))
         };
 
--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
+use std::{error::Error, fs};
 
 use crate::commands_generate::GenerateCommand;
 use clap::Subcommand;
+use parser::{CQRLParser, API};
 use server::Server;
 
 #[derive(Debug, Clone, Subcommand)]
@@ -10,15 +11,44 @@ pub(crate) enum Commands {
         #[command(subcommand)]
         command: GenerateCommand,
     },
-    Serve,
+    Serve {
+        #[arg(long, short, default_value_t = String::from("./service.cqrl"))]
+        input: String,
+    },
 }
 
 impl Commands {
     pub(crate) async fn run(self: Self) -> Result<(), Box<dyn Error>> {
         match self {
             Commands::Generate { command } => command.run().await,
-            Commands::Serve => {
-                let server = Server::new();
+            Commands::Serve { input } => {
+                {
+                    println!("Serving CQRL for {}", input);
+                }
+                let mut content: String = String::new();
+
+                match fs::read_to_string(input.clone()) {
+                    Ok(file) => content = file,
+                    Err(err) => {
+                        println!("Cannot read input file `{}`: {}", input, err);
+                    }
+                };
+
+                let mut api: API = API::new();
+
+                match CQRLParser::parse_string(&content) {
+                    Ok(parsed_api) => {
+                        api = parsed_api;
+                    }
+                    Err(err) => {
+                        println!("Cannot parse input file: `{}`: {}", input, err);
+                    }
+                };
+
+                let mut server = Server::new();
+
+                server.with_api(api);
+
                 server.serve().await
             }
         }
