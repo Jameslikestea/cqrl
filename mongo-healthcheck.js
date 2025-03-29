c0631a68f16f99243d44eb9859e4bb07a772a142
James Crocker <5156202+Jameslikestea@users.noreply.github.com>
2025-03-29 16:44:51 +0000

    update replica set initialisation

---
+++ mongo-healthcheck.js
@@ -0,0 +1,13 @@
+try {
+    rs.status();
+} catch (err) {
+    rs.initiate({
+        _id: "rs0",
+        members: [
+            {
+                _id: 0,
+                host: "localhost:27017"
+            }
+        ]
+    });
+}
