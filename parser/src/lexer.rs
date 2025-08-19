96318db5060975ce3ca131e2001813d45e3d3669
James Crocker <5156202+Jameslikestea@users.noreply.github.com>
2025-08-19 22:28:10 +0100

    fix issue with elided lifetime

--- parser/src/lexer.rs
+++ parser/src/lexer.rs
@@ -1,6 +1,6 @@
 use pest::{iterators::Pairs, Parser};
 
-pub fn lex_tokens(input: &str) -> Pairs<crate::Rule> {
+pub fn lex_tokens(input: &str) -> Pairs<'_, crate::Rule> {
     let output = crate::CQRLParser::parse(crate::Rule::document, input);
     match output {
         Ok(pairs) => return pairs,
